8e3523c1c5ac02fb8a86a33a956e6fd4dd3c20f8
dennism1997 <D.Mouwen@student.tudelft.nl>
2016-12-27 23:14:42 +0100

    places kinda working

--- app/src/main/java/com/moumou/locate/MainActivity.java
+++ app/src/main/java/com/moumou/locate/MainActivity.java
@@ -1,20 +1,45 @@
 package com.moumou.locate;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionApi;
+import com.google.android.gms.location.places.PlaceLikelihood;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private static final int RC_LOCATION = 9002;
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "REQ_UPDATES";
+    private static final String LOCATION_KEY = "LOCATION";
     private String email;
 
     //FAB's
@@ -28,11 +53,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Animation rotate_forward;
     private Animation rotate_backward;
 
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private LocationRequest mLocationRequest;
+    private boolean mRequestingLocationUpdates;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(this);
         fab1 = (FloatingActionButton) findViewById(R.id.fab1);
@@ -47,29 +77,70 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rotate_forward = AnimationUtils.loadAnimation(this, R.anim.rotate_forward);
         rotate_backward = AnimationUtils.loadAnimation(this, R.anim.rotate_backward);
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .build();
+        }
+
+
+        updateValuesFromBundle(savedInstanceState);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         Intent i = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(i);
-
-        SharedPreferences sharedPreferences = PreferenceManager
-                .getDefaultSharedPreferences(getBaseContext());
-        if(sharedPreferences.getString("email", null) == null){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
+                getBaseContext());
+        if (sharedPreferences.getString("email", null) == null) {
             startActivity(i);
         } else {
             email = sharedPreferences.getString("email", null);
+            Log.d("SHAREPREF", "email:" + email);
+            mGoogleApiClient.connect();
         }
-
-
     }
 
     @Override
     public void onStop() {
+        mGoogleApiClient.disconnect();
         super.onStop();
         closeFAB();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+//        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+//                                      mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Update the value of mCurrentLocation from the Bundle and update the
+            // UI to show the correct latitude and longitude.
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mCurrentLocationis not null.
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+
+
+        }
     }
 
     @Override
@@ -128,6 +199,90 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this,
+                                               android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                                                                                    android.Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                                                 PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                                              new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                                              RC_LOCATION);
+            return;
+        }
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mCurrentLocation != null) {
+            Log.d("LOCATION", "current location: " + mCurrentLocation.toString());
+            startLocationUpdates();
+        }
+    }
 
+    @Override
+    public void onConnectionSuspended(int i) {
+        if (i == CAUSE_SERVICE_DISCONNECTED) {
+            Toast.makeText(this, "Disconnected. Please re-connect.", Toast.LENGTH_SHORT).show();
+        } else if (i == CAUSE_NETWORK_LOST) {
+            Toast.makeText(this, "Network lost. Please re-connect.", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    // Trigger new location updates at interval
+    protected void startLocationUpdates() {
+        // Create the location request
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10*60000);
+        mLocationRequest.setFastestInterval(5*60000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        // Request location updates
+
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                                                                     mLocationRequest,
+                                                                     this);
+        } catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        getPlaces();
+        Log.d("LOCATION", "[" + new Date() + "]" + location.toString());
+    }
+
+    private List<Place> getPlaces(){
+        final List<Place> places = new ArrayList<>();
+        try{
+        PendingResult<PlaceLikelihoodBuffer> result = Places.PlaceDetectionApi
+                .getCurrentPlace(mGoogleApiClient, null);
+        result.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(@NonNull PlaceLikelihoodBuffer likelyPlaces) {
+                for (PlaceLikelihood placeLikelihood : likelyPlaces) {
+
+                    if(placeLikelihood.getLikelihood() > 0.1) {
+                        places.add(placeLikelihood.getPlace());
+                        Log.i("PLACES", String.format("Place '%s' has likelihood: %g",
+                                                      placeLikelihood.getPlace().getName(),
+                                                      placeLikelihood.getLikelihood()));
+                    }
+                }
+                likelyPlaces.release();
+            }
+        });
+
+            return places;
+
+        } catch (SecurityException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
